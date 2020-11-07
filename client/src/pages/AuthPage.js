0ee41d94968e08d0698a2d480ec0448b4059de64
dyriavin <Dyriavin@gmail.com>
2020-11-08 00:35:58 +0200

    Auth component: - Styles - HTMl tree - Basic inputs

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,8 +2,45 @@ import React from 'react'
 
 export const AuthPage = () => {
     return (
-        <div>
-            <h1>Auth page</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Learning Node/React</h1>
+                <div className="card blue darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Auth</span>
+                        <div>
+                            <div className="input-field">
+                                <input placeholder="Type your email "
+                                       id="email"
+                                       type="email"
+                                       name="email"
+                                       className="validate yellow-input"
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input placeholder="Type your password "
+                                       id="password"
+                                       type="password"
+                                       name="password"
+                                       className="validate yellow-input"
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn yellow darken-4"
+                                style={{marginRight: 10}}>
+                            Login
+                        </button>
+                        <button className="btn grey lighten-1 black-text">
+                            Register
+                        </button>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
